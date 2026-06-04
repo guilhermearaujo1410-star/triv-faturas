3c9c0ba323ef7836a066c2feae355fe3c18e14aa
guilhermearaujo1410-star <guilhermearaujo1410@gmail.com>
2026-06-04 14:28:43 -0300

    Delete triv-faturas 2 directory

--- triv-faturas 2/app/api/invoices/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-import { getInvoices } from '@/lib/data'
-
-export async function GET() {
-  const invoices = await getInvoices()
-  // Sort by due date desc
-  invoices.sort((a, b) => b.dueDate.localeCompare(a.dueDate))
-  return NextResponse.json(invoices)
-}
